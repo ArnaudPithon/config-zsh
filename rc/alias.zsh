81babbad936d470581bbcedd36b96ab59e191130
bildoon <bildoon@free.fr>
2013-12-01 03:29:27 +0100

    alias alternatifs à cp et mv avec barre de progression

--- rc/alias.zsh
+++ rc/alias.zsh
@@ -20,3 +20,6 @@ alias here='rep=$(basename $PWD) ; eval $rep=$PWD ; echo ~$rep > /dev/null'
 
 # Permet de sauvegarder les attachements dans le répertoire Desktop.
 alias mutt='cd ~/Desktop ; mutt ; cd - > /dev/null'
+
+alias ccp='/usr/bin/rsync --times --recursive --progress'
+alias cmv='/usr/bin/rsync --times --recursive --progress --remove-source-files'
