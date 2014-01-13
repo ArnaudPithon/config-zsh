f35bdfc86808db1eb22b24e0a9ce45eb995d2f92
bildoon <bildoon@free.fr>
2014-01-13 06:30:48 +0100

    Correction du commit précédent.
    Quel idiot, ma modification n'activait pas l'option.

--- rc/history.zsh
+++ rc/history.zsh
@@ -10,13 +10,4 @@ setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
-# Correctif temporaire tant que ce bug est présent
-# http://www.zsh.org/mla/users/2013/msg00456.html
-# <url:man:zshoptions#tn=HIST_REDUCE_BLANKS>
-#if [[ `uname -m` == 'x86_64' ]]; then
-#	setopt NO_HIST_REDUCE_BLANKS
-#else
-#	setopt HIST_REDUCE_BLANKS
-#fi
-# Semble être devenu inutile depuis la dernière mise à jour.
-# 2013-01-13 zsh 5.0.5
+setopt HIST_REDUCE_BLANKS
